abcdab8561c2741147a38cd939ab796c3e843a26
allenye0909 <37999330+allenye0909@users.noreply.github.com>
2021-12-28 14:16:47 +0800

    做 console

--- utils.js
+++ utils.js
@@ -17,11 +17,22 @@ var Utils = (function (exports) {
         }, duration);
     }
 
+    function getQueryVariable(variable) {
+        var query = window.location.search.substring(1);
+        var vars = query.split("&");
+        for (var i = 0; i < vars.length; i++) {
+            var pair = vars[i].split("=");
+            if (pair[0] == variable) { return pair[1]; }
+        }
+        return (false);
+    }
+
     const log = (tag, ...args) => {
         console.log(tag, new Date().toISOString(), ...args)
     }
 
     exports.toast = toast;
+    exports.getQueryVariable = getQueryVariable;
     exports.log = log;
 
     Object.defineProperty(exports, '__esModule', { value: true });
